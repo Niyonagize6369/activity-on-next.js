bd2e44d004fa65116142631c757082ee5ef81986
Your Name <niyonagizerachel10@email.com>
2025-06-10 19:46:53 +0200

    next.js revision

--- app/Users/page.tsx
+++ app/Users/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Userspage = () => {
+interface User{
+  id:number,
+  name:string,
+
+}
+const Userspage = async() => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/users',
+    {next: {revalidate: 10 }});
+ const users: User[] = await res.json();
   return (
     <div>
-      UsersPage
+      <h1>Users</h1>
+      <ul>
+        {users.map(User => <li key={User.id}>{User.name}</li>)}
+      </ul>
     </div>
   )
 }
